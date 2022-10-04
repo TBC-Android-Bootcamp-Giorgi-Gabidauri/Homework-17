4b45fdd2a38e8cc1de729af801978cf8658f3fb2
Giorgi Gabidauri <giorgi.gabidauri16@gmail.com>
2022-10-05 02:48:35 +0400

    initial commit

---
+++ app/src/main/java/com/gabo/navdrawer/ListProvider.kt
@@ -0,0 +1,12 @@
+package com.gabo.navdrawer
+
+object ListProvider {
+    val menuList = listOf(
+        MenuModel(R.drawable.ic_dashboard, "Dashboard", "", null),
+        MenuModel(R.drawable.ic_mail, "Pesan", " 15 ", R.drawable.bg_number_orange),
+        MenuModel(R.drawable.ic_notifications, "Notifikasi", " 20 ", R.drawable.bg_number_green),
+        MenuModel(R.drawable.ic_calendar, "Kalendar", "", null),
+        MenuModel(R.drawable.ic_statistics, "Statistik", "", null),
+        MenuModel(R.drawable.ic_settings, "Pergaturan", "", null),
+    )
+}
---
+++ app/src/main/java/com/gabo/navdrawer/MenuModel.kt
@@ -0,0 +1,8 @@
+package com.gabo.navdrawer
+
+data class MenuModel(
+    val icon: Int,
+    val title: String,
+    val number: String,
+    val numColor: Int?
+)
